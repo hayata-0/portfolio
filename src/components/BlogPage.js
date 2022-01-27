2698569c9ad3dd54b493ac3d33a2bf3043e956bd
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 11:14:32 +0900

    各コンポーネント作成

---
+++ src/components/BlogPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogPage = () => {
+  return <div>BlogPage component/Page</div>;
+};
+
+export default BlogPage;
---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MySkillsPage = () => {
+  return <div>MySkillsPage component/Page</div>;
+};
+
+export default MySkillsPage;
