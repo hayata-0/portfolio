bc130ba3ff3e072d3987bc401eaa8b7dc7f5ca9c
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 19:36:49 +0900

    PowerButton.jsのStyleを追記

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PowerButton from "../subComponents/PowerButton";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
@@ -25,7 +26,9 @@ const Container = styled.div`
 const Main = () => {
   return (
     <MainContainer>
-      <Container>Main component/Page</Container>
+      <Container>
+        <PowerButton />
+      </Container>
     </MainContainer>
   );
 };
--- src/subComponents/PowerButton.js
+++ src/subComponents/PowerButton.js
@@ -8,6 +8,13 @@ const Power = styled.button`
   top: 2rem;
   left: 50%;
   transform: translate(-50%, 0);
+
+  background-color: #fcf6f4;
+  padding: 0.3rem;
+  border-radius: 50%;
+  border: 1px solid #000;
+  width: 2.5rem;
+  height: 2.5rem;
 `;
 
 const PowerButton = () => {
