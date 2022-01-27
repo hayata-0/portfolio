aba37f652856ac93fc487ec9424343a39f47e88d
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 11:32:08 +0900

    各コンポーネントをRouteで指定をした

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { Route, Switch } from "react-router";
 import { ThemeProvider } from "styled-components";
 import { LightTheme } from "./components/Themes";
 import GlobalStyle from "./globalStyles";
+
+//Components
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={LightTheme}>App File</ThemeProvider>
+      <ThemeProvider theme={LightTheme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/work" component={WorkPage} />
+          <Route exact path="/skills" component={MySkillsPage} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
