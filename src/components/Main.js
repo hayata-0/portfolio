e773363ad17aaec612c623ee8a9dfe187bb2c2d1
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 23:48:20 +0900

    それぞれのNavLinkを配置

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,6 +31,50 @@ const Contact = styled(NavLink)`
   position: absolute;
   top: 2rem;
   right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const WORK = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: calc(1rem + 2vw);
+  transform: translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const ABOUT = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const SKILLS = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
   z-index: 1;
 `;
 
@@ -46,8 +90,22 @@ const Main = () => {
           target="_blank"
           to={{ pathname: "mailto:hayata1454@gmail.com" }}
         >
-          <h3>Contact</h3>
+          <h2>Contact</h2>
         </Contact>
+        <BLOG to="/blog">
+          <h2>Blog</h2>
+        </BLOG>
+        <WORK to="/work">
+          <h2>Work</h2>
+        </WORK>
+        <BottomBar>
+          <ABOUT to="/about">
+            <h2>About</h2>
+          </ABOUT>
+          <SKILLS to="/skills">
+            <h2>My Skill</h2>
+          </SKILLS>
+        </BottomBar>
       </Container>
     </MainContainer>
   );
