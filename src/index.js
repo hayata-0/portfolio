0cae786b0fb7dd970d04e76043517c34665225e4
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 11:17:37 +0900

    BrowserRouterをindex.jsにimport

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 import "../node_modules/normalize.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
