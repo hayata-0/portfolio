416b011f094ba507cce582a3de14590a807b7995
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-27 11:47:00 +0900

    Main.jsのStyleを追記

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,31 @@ import styled from "styled-components";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
+
+const Container = styled.div`
+  padding: 2rem;
 `;
 
 const Main = () => {
-  return <MainContainer>Main component/Page</MainContainer>;
+  return (
+    <MainContainer>
+      <Container>Main component/Page</Container>
+    </MainContainer>
+  );
 };
 
 export default Main;
