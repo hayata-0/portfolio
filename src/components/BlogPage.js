b587da46832c2406eed7ecb61a5924060f2b3851
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-31 23:40:16 +0900

    backgroundImageを設定

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg";
+
+const MainContainer = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+`;
 
 const BlogPage = () => {
-  return <div>BlogPage component/Page</div>;
+  return <MainContainer></MainContainer>;
 };
 
 export default BlogPage;
