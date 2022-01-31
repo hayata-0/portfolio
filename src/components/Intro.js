7ecec0750462f7656cf3eb2083c9d5fe22b6a6ec
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-31 21:17:47 +0900

    profileのデザイン実装

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Me from "../assets/Images/profile-pic.png";
 
 const Box = styled.div`
   position: absolute;
@@ -8,10 +9,8 @@ const Box = styled.div`
   transform: translate(-50%, -50%);
 
   width: 55vw;
-  height: 40vh;
   display: flex;
-  background: no-repeat;
-  background-size: 100% 2px;
+
   background: linear-gradient(
         to right,
         ${(props) => props.theme.body} 50%,
@@ -24,14 +23,58 @@ const Box = styled.div`
         ${(props) => props.theme.text} 50%
       )
       top;
+  background-repeat: no-repeat;
+  background-size: 100% 2px;
 
   border-left: 2px solid ${(props) => props.theme.body};
   border-right: 2px solid ${(props) => props.theme.text};
+
   z-index: 1;
 `;
 
+const SubBox = styled.div`
+  width: 50%;
+  position: relative;
+  display: flex;
+
+  .pic {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const Text = styled.div`
+  font-size: calc(1em + 1.5vw);
+  color: ${(props) => props.theme.body};
+  padding: 2rem;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
 const Intro = () => {
-  return <Box>Introduction</Box>;
+  return (
+    <Box>
+      <SubBox>
+        <Text>
+          <h1>Hi,</h1>
+          <h3>I'm HayataOhno</h3>
+          <h6>Welcome to My Profile</h6>
+        </Text>
+      </SubBox>
+      <SubBox>
+        <div>
+          <img className="pic" src={Me} alt="Profile Pic" />
+        </div>
+      </SubBox>
+    </Box>
+  );
 };
 
 export default Intro;
