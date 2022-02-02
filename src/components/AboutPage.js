32e4442db203ead71729a2e3d4b399c6bedc94ab
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-02 22:35:42 +0900

    AboutPageの大元作成

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes.js";
+import { Design, Develope } from "./AllSvgs";
+
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
 
 const AboutPage = () => {
-  return <div>AboutPage component/Page</div>;
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme="dark" />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default AboutPage;
