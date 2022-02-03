f8e4fed25325cb558e6cc5985452a9f041117090
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-04 00:54:12 +0900

    Sound処理のレイアウト設定

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRef } from "react/cjs/react.development";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import music from "../assets/audio/stay-lost-with-me_by_reed-pittman_Artlist.mp3";
 
 const Box = styled.div`
@@ -10,6 +10,45 @@ const Box = styled.div`
   left: 8rem;
   top: 3rem;
   z-index: 10;
+
+  & > *:nth-child(1) {
+    animation-delay: 0.2s;
+  }
+  & > *:nth-child(2) {
+    animation-delay: 0.3s;
+  }
+  & > *:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+  & > *:nth-child(4) {
+    animation-delay: 0.5s;
+  }
+  & > *:nth-child(5) {
+    animation-delay: 0.8s;
+  }
+`;
+
+const play = keyframes`
+0% {
+    transform: scaleY(1);
+}
+50% {
+    transform: scaleY(2);
+}
+100% {
+    transform: scaleY(1);
+}
+`;
+
+const Line = styled.span`
+  background: ${(props) => props.theme.text};
+  border: 1px solid ${(props) => props.theme.body};
+
+  animation: ${play} 1s ease infinite;
+  animation-play-state: ${(props) => (props.click ? "running" : "paused")};
+  height: 1rem;
+  width: 2px;
+  margin: 0 0.1rem;
 `;
 
 const SoundBar = () => {
@@ -28,7 +67,11 @@ const SoundBar = () => {
 
   return (
     <Box onClick={() => handleClick()}>
-      Sound
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
+      <Line click={click} />
       <audio src={music} ref={ref} loop />
     </Box>
   );
