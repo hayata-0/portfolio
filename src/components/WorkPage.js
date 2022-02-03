d951643d991a6c204c000c71d6988eb15cae3cf1
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-03 19:34:11 +0900

    WorkDataをimport

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes.js";
 
-const WorkPage = () => {
-  return <div>WorkPage component/Page</div>;
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+
+import { Work } from "../data/WorkData";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+const WarkPage = () => {
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
-export default WorkPage;
+export default WarkPage;
