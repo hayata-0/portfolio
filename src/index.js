d4787fe021804e62d46e8f176eec0ec3742abd1f
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-06 23:22:49 +0900

    Add new URL

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
